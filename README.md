83d051b82c7830237b868f304fba33e59293a464
André Kilchenmann <github@milchkannen.ch>
2021-09-28 23:09:55 +0200

    improvements

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A 24 hour analog clock for the Fitbit Sense smart watch.
 
-![malrapida clock screenshot](resources/malrapida-clock-screenshot.png)
+![malrapida clock screenshot](resources/icon.png)
--- app/index.js
+++ app/index.js
@@ -1,12 +1,27 @@
+import { me as appbit } from "appbit";
+import { BodyPresenceSensor } from "body-presence";
 import clock from "clock";
+import { display } from "display";
 import * as document from "document";
 import { HeartRateSensor } from "heart-rate";
-import { battery, charger } from "power";
-import { display } from "display";
-import { me as appbit } from "appbit";
+import { battery } from "power";
 import { today } from "user-activity";
 
-let minutehand = document.getElementById("minutehand");
+// definitions firs
+const weekday = new Array(7);
+weekday[0] = "Su";
+weekday[1] = "Mo";
+weekday[2] = "Tu";
+weekday[3] = "We";
+weekday[4] = "Th";
+weekday[5] = "Fr";
+weekday[6] = "Sa";
+
+const minuteHand = document.getElementById("minute-hand");
+const heartLabel = document.getElementById("heart-label");
+const stepsLabel = document.getElementById("steps-label");
+const dateLabel = document.getElementById("date-label");
+const powerLabel = document.getElementById("power-label");
 
 // Update the clock every minute
 clock.granularity = "minutes";
@@ -14,42 +29,40 @@ clock.granularity = "minutes";
 clock.ontick = (evt) => {
    // set position of minutes-hand
    let degree = (evt.date.getHours() * 15) + (evt.date.getMinutes() * .25) - 180;
-   minutehand.groupTransform.rotate.angle = degree;
+   minuteHand.groupTransform.rotate.angle = degree;
 };
 
+// heart rate
 if (HeartRateSensor) {
-   const heartLabel = document.getElementById("heart-label");
-   const hrm = new HeartRateSensor();
+
+   let hrm = new HeartRateSensor();
    hrm.addEventListener("reading", () => {
       heartLabel.text = hrm.heartRate;
    });
-   // Automatically stop the sensor when the screen is off to conserve battery.
-   display.addEventListener("change", () => {
-      display.on ? hrm.start() : hrm.stop();
+
+   let body = new BodyPresenceSensor();
+   body.addEventListener("reading", () => {
+      if (!body.present) {
+         hrm.stop();
+         heartLabel.text = "--";
+      } else {
+         hrm.start();
+      }
    });
-   hrm.start();
+   body.start();
+
 }
 
+// steps
 if (appbit.permissions.granted("access_activity")) {
-   const stepsLabel = document.getElementById("steps-label");
-   stepsLabel.text = Math.floor(today.adjusted.steps / 1000, 0) + "k";
+   stepsLabel.text = Math.floor((today.adjusted.steps / 1000) * 100) / 100 + "k";
 }
 
-const powerLabel = document.getElementById("power-label");
+// power
 const powerSymbol = (battery.charging ? "+" : "%");
 powerLabel.text = Math.floor(battery.chargeLevel) + powerSymbol;
 
-const dayLabel = document.getElementById("day-label");
-
+// weekday and day
 const d = new Date();
-let weekday = new Array(7);
-weekday[0] = "S";
-weekday[1] = "M";
-weekday[2] = "T";
-weekday[3] = "W";
-weekday[4] = "D";
-weekday[5] = "F";
-weekday[6] = "S";
-
 const n = weekday[d.getDay()];
-dayLabel.text = n + "" + d.getDate();
+dateLabel.text = n + " " + d.getDate();
